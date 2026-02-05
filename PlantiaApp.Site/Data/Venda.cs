62fcf24a848ab02226bb7e33c07a89a2d05a9de7
felipe bastos <felipebastosweb@gmail.com>
2026-02-05 17:29:42 -0300

    Enhance Venda and VendaItem classes with new properties
    Added properties and relationships for Venda and VendaItem classes.

--- PlantiaApp.Site/Data/Venda.cs
+++ PlantiaApp.Site/Data/Venda.cs
@@ -3,5 +3,25 @@
 public class Venda
 {
     public Guid Id { get; set; }
+    public Guid ClienteId { get; set; }
+    [ForeignKey(nameof(ClienteId))]
+    public virtual Cliente Cliente { get; set; } = null!;
+    public decimal Total { get; set; }
+    public ICollection<VendaItem> VendaItems { get; set; } = [];
+}
 
+
+public class VendaItem
+{
+    public Guid Id { get; set; }
+    public Guid VendaId { get; set; }
+    [ForeignKey(nameof(VendaId))]
+    public virtual Venda Venda { get; set; } = null
+    public Guid CompraItemId { get; set; }
+    [ForeignKey(nameof(CompraItemId))]
+    public virtual CompraItem CompraItem { get; set; } = null!;
+    public decimal PrecoUnitario { get; set; }
+    public decimal Quantidade { get; set; }
+    public decimal Total { get; set; }
+    public ICollection<VendaItem> VendaItems { get; set; } = [];
 }
